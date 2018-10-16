637af3a175d25c571f6bf2b20bea5e97ad939a6c
Alan Wong <alanwong2357@gmail.com>
2018-10-15 22:57:13 -0700

    updated board bios and pictures

--- src/app/projects.component.ts
+++ src/app/projects.component.ts
@@ -8,17 +8,22 @@ import * as M from '../../node_modules/materialize-css/dist/js/materialize.min.j
 })
 
 export class ProjectsComponent implements OnInit {
-  // options = [indicators = true];
   options = {};
   elems = {};
-
 
   ngOnInit() {
     var elems = document.querySelectorAll('.slider');
     var instances = M.Slider.init(elems, this.options);
+  }
 
-    var elems1 = document.querySelectorAll('.carousel');
-    var instances1 = M.Carousel.init(elems1, this.options);
+  // goTo(location: string): void {
+  //   window.location.hash = location;
+  //   window.location.hash = ''; window.location.hash = location;
+  // }
+
+  scrollToElement($element): void {
+    console.log($element);
+    $element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   }
 
 }
